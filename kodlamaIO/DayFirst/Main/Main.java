7aab0415e021dbc431d3cd4fbabc5e34e1208a38
Bedirhan <20200808033@ogr.akdeniz.edu.tr>
2022-03-07 22:13:41 +0300

    Main.java directory changed

---
+++ kodlamaIO/DayFirst/Main/Main.java
@@ -0,0 +1,14 @@
+package kodlamaIO.DayFirst.Main;
+
+import java.util.Scanner;
+/**
+ * @author Bedirhan Tong
+ */
+
+class Main{
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Hello World!");
+        sc.close();
+    }
+}
