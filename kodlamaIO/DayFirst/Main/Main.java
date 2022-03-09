6871875e40e7b6b72e0c86233ff748150b7d8e98
Bedirhan <20200808033@ogr.akdeniz.edu.tr>
2022-03-09 23:08:19 +0300

    Examples added

--- kodlamaIO/DayFirst/Main/Main.java
+++ kodlamaIO/DayFirst/Main/Main.java
@@ -9,6 +9,27 @@ class Main{
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
         System.out.println("Hello World!");
+        // Let's see how primitive datatypes work in Java.
+        int i1 = 2;
+        int i2 = 3;
+        i1 = i2;
+        // value copied
+        i2 = 4;
+        System.out.println(i1);
+        // Let's see how referance datatypes work in Java.
+        int[] arr = new int[1];
+        arr[0] = 1;
+        int[] arr2 = new int[1];
+        arr2[0] = 2;
+        arr = arr2;
+        // address copied
+        arr[0] = 3;
+        System.out.println(arr2[0]);
+        String s1 = "Hello";
+        String s2 = "World";
+        s1 = s2;
+        s1 = "bedirhan";
+        System.out.println(s1);
         sc.close();
     }
 }
