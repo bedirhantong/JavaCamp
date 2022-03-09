4721e06a68cd1e8e28f34d5b6224f5d7ed585cf7
Bedirhan <20200808033@ogr.akdeniz.edu.tr>
2022-03-09 23:12:35 +0300

    FirstDayOfCampİsDone

--- kodlamaIO/DayFirst/Main/Main.java
+++ kodlamaIO/DayFirst/Main/Main.java
@@ -14,8 +14,10 @@ class Main{
         int i2 = 3;
         i1 = i2;
         // value copied
-        i2 = 4;
-        System.out.println(i1);
+        i1 = 4;
+        System.out.println(i2);
+
+
         // Let's see how referance datatypes work in Java.
         int[] arr = new int[1];
         arr[0] = 1;
@@ -25,11 +27,13 @@ class Main{
         // address copied
         arr[0] = 3;
         System.out.println(arr2[0]);
+
+        // Let's see how String datatypes work in Java.
         String s1 = "Hello";
         String s2 = "World";
         s1 = s2;
         s1 = "bedirhan";
-        System.out.println(s1);
+        System.out.println(s2);
         sc.close();
     }
 }
